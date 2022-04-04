bce4927fca1c5c579f3ca214cd0a5977d86e0f02
Gustavo Ribeiro Kremer <35006023+grkremer@users.noreply.github.com>
2022-04-04 15:21:23 -0300

    Activity com o container da lista criado

--- app/src/main/java/com/example/ihc_navegador_de_compras_new/TelaListaActivity.java
+++ app/src/main/java/com/example/ihc_navegador_de_compras_new/TelaListaActivity.java
@@ -1,14 +1,14 @@
 package com.example.ihc_navegador_de_compras_new;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaListaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_lista);
     }
 }
