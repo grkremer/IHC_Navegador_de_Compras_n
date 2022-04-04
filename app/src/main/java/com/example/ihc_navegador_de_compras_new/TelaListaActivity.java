bab40b92623b2887a266bdf64b65f296681323ad
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2022-04-04 19:07:39 -0300

    hardcode da lista

--- app/src/main/java/com/example/ihc_navegador_de_compras_new/TelaListaActivity.java
+++ app/src/main/java/com/example/ihc_navegador_de_compras_new/TelaListaActivity.java
@@ -18,8 +18,8 @@ public class TelaListaActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ProductAdapter adapter;
 
-    private List<ProductModel> products;
-
+    //private List<ProductModel> products;
+    int counter = 0;
     private SearchView searchView;
 
     @Override
@@ -34,18 +34,48 @@ public class TelaListaActivity extends AppCompatActivity {
         adapter = new ProductAdapter(TelaListaActivity.this);
         recyclerView.setAdapter(adapter);
 
-        products = new ArrayList<ProductModel>();
-        products.add(new ProductModel("Uva"));
-        products.add(new ProductModel("Suco de Uva"));
+        List<ProductModel> products1 = new ArrayList<ProductModel>();
+        products1.add(new ProductModel("Uva"));
+        products1.add(new ProductModel("Suco de Uva"));
+
+        List<ProductModel> products2 = new ArrayList<ProductModel>();
+        products2.add(new ProductModel("Queijo Parmesão"));
+        products2.add(new ProductModel("Queijo Mussarela"));
+        products2.add(new ProductModel("Queijo Cheddar"));
+        products2.add(new ProductModel("Queijo Prato"));
+
+        List<ProductModel> products3 = new ArrayList<ProductModel>();
+        products3.add(new ProductModel("Arroz Branco"));
+        products3.add(new ProductModel("Arroz Integral"));
+        products3.add(new ProductModel("Arroz Parabolizado"));
+
+
 
         searchView = findViewById(R.id.searchView);
-        searchView.setOnSearchClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                       adapter.setProducts(products);
-                    }
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                switch (counter) {
+                    case 0:
+                        adapter.setProducts(products1);
+                        break;
+                    case 1:
+                        adapter.setProducts(products2);
+                        break;
+                    default:
+                        adapter.setProducts(products3);
+                        break;
                 }
-        );
+                counter++;
+                //adapter.setProducts(products);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
 }
